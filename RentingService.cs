40456f1c90e1991320e0e0ce7d97b23fcca7f857
olemartin25sfj <olemartin25fj@gmail.com>
2025-01-29 09:22:40 +0100

    fix initialization of renting service

--- RentingService.cs
+++ RentingService.cs
@@ -5,15 +5,22 @@ class RentingService
 
     public RentingService()
     {
-        bookInventory = new Dictionary<Book, int>{
-      { new Book("Martian", "Jim"), 10 },
-      { new Book("Foundation", "Jack"), 30 },
-    };
-        ;
+        // Lage ett sett med bøker
+        Book martian = new Book("Martian", "Jim");
+        Book foundation = new Book("Foundation", "Jack");
 
+
+        // Opprette datastrukturen
+        bookInventory = new Dictionary<Book, int>();
         currentlyBorrowed = new Dictionary<Book, int>();
-    }
 
+        // Legg til bøker med antall
+
+        bookInventory.Add(martian, 10);
+        currentlyBorrowed.Add(martian, 0);
+        bookInventory.Add(foundation, 2);
+        currentlyBorrowed.Add(foundation, 0);
+    }
     public Dictionary<Book, int> ListAllBooks()
     {
         return bookInventory;
